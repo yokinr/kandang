284de95376563dbc5fddf787a3cd8609dcdaf6c2
yoki <ynrestu@hotmail.com>
2023-01-30 09:46:31 +0700

    5

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://kandang.smkn1payakumbuh.sch.id/';
+$config['base_url'] = 'http://localhost/kandang';
 
 /*
 |--------------------------------------------------------------------------
